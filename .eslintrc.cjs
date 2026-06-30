17ecb5e4eb81ca4878a338ec1633742b3672b2de
Abioladory123 <Abioladory@gmail.com>
2026-06-30 15:18:04 +0100

    chore: init sdk package with tsconfig, tsup build, and lint toolchain

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+  ignorePatterns: ["dist/", "node_modules/", "jest.config.*"],
+};
